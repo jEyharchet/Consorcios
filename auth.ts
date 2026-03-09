c073818a7524401b1ffa76f0df57328ba4ac6278
jEyharchet <jee.u21@gmail.com>
2026-03-09 10:49:11 -0300

    Cast Prisma adapter to NextAuth Adapter

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
 ﻿import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-
+import type { Adapter } from "next-auth/adapters";
 import authConfig from "./auth.config";
 import { prisma } from "./src/lib/prisma";
 import { type GlobalRole } from "./src/lib/roles";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   ...authConfig,
   callbacks: {
     ...authConfig.callbacks,
