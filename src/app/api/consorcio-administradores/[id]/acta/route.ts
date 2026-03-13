0a0abd4881baf9bc7f1bde8546c916d790415437
jEyharchet <jee.u21@gmail.com>
2026-03-12 21:55:12 -0300

    Fix auth import in acta route

--- src/app/api/consorcio-administradores/[id]/acta/route.ts
+++ src/app/api/consorcio-administradores/[id]/acta/route.ts
@@ -1,4 +1,4 @@
-import { auth } from '../../../../../../../auth';
+import { auth } from '../../../../../../auth';
 
 import { hasConsorcioAccessForUserId } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
