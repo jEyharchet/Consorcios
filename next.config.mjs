464952a8191c835a612060633a7d2f6bc90b06f8
jEyharchet <jee.u21@gmail.com>
2026-03-15 17:12:39 -0300

    fix(pdf): install chrome for puppeteer in vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": ["./.cache/puppeteer/**/*"],
+    },
+  },
+};
 
 export default nextConfig;
